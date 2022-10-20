c9c23847ee20a680de9a7ee76f0ac09b3c540f69
GabrielaFurtado <115599756+GabrielaFurtado@users.noreply.github.com>
2022-10-19 22:09:48 -0300

    Create MéquiBox.java

---
+++ MéquiBox.java
@@ -0,0 +1,18 @@
+# ProjetoMequi
+package projetoMequi;
+
+import java.util.Scanner;
+
+public class MéquiBox {
+
+	 public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println ("Selecione a opção MéquiBox");
+		 System.out.println  ("Family Box para 2 ");
+		  System.out.println  ("Family Box para 3");
+		   System.out.println  ("Méqui Box Clássico para 4");
+			System.out.println  ("Méqui Box Clássico para 3");
+	}
+
+}

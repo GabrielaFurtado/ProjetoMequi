f2e10de4e50854d5799663d1bf7c6b467f49c963
GabrielaFurtado <115599756+GabrielaFurtado@users.noreply.github.com>
2022-10-19 22:11:24 -0300

    Create TrabalheConosco.java

---
+++ TrabalheConosco.java
@@ -0,0 +1,38 @@
+# ProjetoMequi
+package projetoMequi;
+
+import java.util.Scanner;
+
+
+public class TrabalheConosco {
+
+	public static void main(String[] args) {
+		  System.out.println("Trabalhe conosco"); 
+
+		  System.out.println ("Com mais de 50 mil funcionários no Brasil,"); 
+
+		  System.out.println ("o McDonald's é um dos maiores empregadores de jovens do país"); 
+
+		  System.out.println ("Todos os anos, realizamos o sonho do primeiro emprego de muita gente!"); 
+
+		  System.out.println ("Do nosso atual quadro de funcionários, cerca de 76%"); 
+
+		  System.out.println ("têm menos de 25 anos de idade e estão em sua primeira experiência."); 
+
+		  System.out.println ("Somos uma das empresas que mais oferece chances de ascensão profissional."); 
+
+		  System.out.println ("Pois, quem trabalha com a gente tem várias oportunidades de desenvolvimento."); 
+
+		  System.out.println ("Incentivamos o estudo e o aprimoramento, e temos orgulho de dizer que hoje aqui no Brasil, mais da metade dos gerentes ingressaram na companhia como atendentes,"); 
+
+		  System.out.println ("o primeiro degrau na escala hierárquica da empresa"); 
+
+		  System.out.println("Anualmente, são investidos cerca de R$40 milhões em treinamento e desenvolvimento de funcionários"); 
+
+		  System.out.println (" Aqui, todos têm espaço e oportunidade, não admitimos discriminação e exigimos o mesmo respeito para todos os nossos funcionários,");  
+
+		  System.out.println ("sem qualquer distinção de sexo, idade, raça, orientação sexual, religião ou aparência física"); 
+
+	}
+
+}

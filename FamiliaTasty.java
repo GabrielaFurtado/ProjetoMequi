b2de7798bda536a32be2be29b80d8de146ed4200
GabrielaFurtado <115599756+GabrielaFurtado@users.noreply.github.com>
2022-10-19 22:03:34 -0300

    Create FamiliaTasty.java

---
+++ FamiliaTasty.java
@@ -0,0 +1,14 @@
+# ProjetoMequi
+package projetoMequi;
+
+import java.util.Scanner;
+
+public class FamiliaTasty {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println ("Selecione opção a Família Tasty");
+        System.out.println ("BigTasty");
+        System.out.println  ("BigTasty Turbo Bacon");

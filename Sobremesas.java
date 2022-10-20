b7bc2e59442a2193f0e2204e7931c5cec082d507
GabrielaFurtado <115599756+GabrielaFurtado@users.noreply.github.com>
2022-10-19 22:04:11 -0300

    Create Sobremesas.java

---
+++ Sobremesas.java
@@ -0,0 +1,41 @@
+# ProjetoMequi
+package projetoMequi;
+
+import java.util.Scanner;
+
+public class Sobremesas {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println ("Selecione a opção Sobremesas");
+		System.out.println  ("Casquinha Baunilha");
+		System.out.println  ("Casquinha Chocolate");
+		System.out.println  ("Casquinha Mista");
+		System.out.println  ("Sundae Morango");
+		System.out.println  ("Sundae Chocolate");
+		System.out.println  ("Sundae Caramelo");
+		System.out.println ("Top Sundae Morango");
+		System.out.println ("Top Sundae Chocolate");
+		System.out.println  ("Top Sundae Caramelo");
+		System.out.println ("McColosso Chocolate");
+		System.out.println ("McColosso Caramelo");
+		System.out.println ("Petit Suisse Sabor Morango");
+		System.out.println ("Torta de Maça");
+		System.out.println ("Torta de Banana");
+		System.out.println ("McShake Morango");
+		System.out.println ("McShake Ovomaltine");
+		System.out.println ("McShake Kopenhagen");
+		System.out.println ("McFlurry M&Ms");
+		System.out.println ("McFlurry Espetáculo Kopenhagen Chocolate");
+		System.out.println ("McFlurry Espetáculo Kopenhagen Morango");
+		System.out.println ("McFlurry Espetáculo Kopenhagen Caramelo");
+		System.out.println ("McFlurry Ovomaltine");
+		System.out.println ("McFlurry Prestígio Chocolate");
+		System.out.println ("McFlurry Prestígio Morango");
+		System.out.println ("McFlurry Prestígio Caramelo");
+
+	}
+
+}

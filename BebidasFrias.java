5d4747ff9c5b9ee17456b8983baa0f8906d0bac5
GabrielaFurtado <115599756+GabrielaFurtado@users.noreply.github.com>
2022-10-19 22:05:55 -0300

    Create BebidasFrias.java

---
+++ BebidasFrias.java
@@ -0,0 +1,36 @@
+# ProjetoMequi
+package projetoMequi;
+
+import java.util.Scanner;
+
+public class BebidasFrias {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println ("Selecione a opção Bebidas Frias");
+		System.out.println  ("Coca-Cola 300mL");
+		System.out.println  ("Fanta Laranja 500ml");
+		System.out.println  ("Fanta Laranja 700ml");
+		System.out.println  ("Coca-Cola 500ml");
+		System.out.println  ("Coca-Cola 700ml");
+		System.out.println  ("Coca-Cola Zero 300ml");
+		System.out.println ("Coca-Cola Zero 500ml");
+		System.out.println ("Coca-Cola Zero 700ml");
+		System.out.println  ("Fanta Laranja 300ml");
+		System.out.println ("Fanta Guaraná 300ml");
+		System.out.println ("Fanta Guaraná 500ml");
+		System.out.println ("Fanta Guaraná 700ml");
+		System.out.println ("Del Valle Laranja 300ml");
+		System.out.println ("Del Valle Laranja 500ml");
+		System.out.println ("Del Valle Laranja 700ml");
+		System.out.println ("Del Valle Uva 300ml");
+		System.out.println ("Del Valle Uva 500ml");
+		System.out.println ("Del Valle Uva 700ml");
+		System.out.println ("Água Mineral");
+		System.out.println ("Del Valle 100% Uva McLanche Feliz");
+
+	}
+
+}

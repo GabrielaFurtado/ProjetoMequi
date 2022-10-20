19c5aaf363a64c4364f994f438b313d0eb58f3dd
GabrielaFurtado <115599756+GabrielaFurtado@users.noreply.github.com>
2022-10-20 00:26:24 -0300

    Create QuemSomos.java

---
+++ QuemSomos.java
@@ -0,0 +1,125 @@
+# ProjetoMequi
+package projetoMequi;
+
+import java.util.Scanner;
+
+public class QuemSomos {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+    
+		
+		System.out.println("Institucional");
+		System.out.println();
+		System.out.println("A Arcos Dorados é a maior franquia independente direitos exclusivos de possuir, operar e conceder o McDonald’s do mundo e a principal rede de alimentação rápida em toda América Latina e Caribe. A");
+		System.out.println("companhia tem as franquias de restaurantes McDonald's em 20 países e territórios, incluindo Argentina, Aruba, Brasil, Chile, Colômbia, Costa Rica, Curaçao, Equador, Guiana Francesa, Guadalupe, Martinica,");
+		System.out.println("México, Panamá, Peru, Porto Rico, St. Croix, St. Thomas, Trinidad & Tobago, Uruguai e Venezuela.");
+		System.out.println();
+		System.out.println("Desde sua fundação, em 2007, a companhia preza por compromissos que norteiam sua operação, como qualidade, transparência e segurança do alimento, geração de empregos formais para jovens, apoio às");
+		System.out.println("comunidades onde está presente, cadeia de suprimentos sustentável e impacto ambiental, diversidade e inclusão e experiências inovadoras para toda a família.");
+		System.out.println();
+		System.out.println("Atualmente, a rede conta com mais de 2.200 restaurantes em toda América Latina, entre unidades próprias e subfranqueadas, além de mais de 240 McCafés e cerca de 3.300 desert centers. Para isso, contamos");
+		System.out.println("com mais de 100 mil funcionários em toda região. No Brasil, são cerca de 1.020 restaurantes, mais de 75 McCafés e 2.010 desert centers, empregando mais de 50 mil pessoas.");
+		System.out.println();
+		System.out.println("Para saber mais sobre a companhia, visite o site: www.arcosdorados.com.");
+		
+			
+		System.out.println("Franquias");
+		System.out.println();
+		System.out.println("Líder absoluto no segmento de alimentação rápida e com produtos de qualidade reconhecida, o McDonald’s é uma das melhores franquias para investir e administrar. O modelo de negócios apoiado pelas");
+		System.out.println("franquias foi o fator que permitiu ao McDonald’s se tornar a rede de restaurantes de maior sucesso do mundo.");
+		System.out.println();
+		System.out.println("Entre os motivos que fazem do McDonald’s líder em seu segmento, estão:");
+		System.out.println(" Marca com reconhecimento mundial");
+		System.out.println(" Modelo de negócios testado e bem-sucedido");
+		System.out.println(" Sistema que proporciona altos níveis de treinamento");
+		System.out.println(" Provedores de excelência");
+		System.out.println("Oferece rede de suporte profissional");
+		System.out.println(" Desde suas origens, desenvolveu as franquias como modelo de negócio predominante");
+		System.out.println();
+		System.out.println("Quero ser um franqueado!");
+		System.out.println("Para entrar em contato com a área de Franquias da Rede, envie um e-mail para infofranquia@br.mcd.com");
+		
+			
+			System.out.println("Trabelhe conosco");
+			System.out.println();
+			System.out.println("Com mais de 50 mil funcionários no Brasil, o McDonald's é um dos maiores empregadores de jovens do país. Todos os anos, realizamos o sonho do primeiro emprego de muita gente! Do nosso atual quadro de");
+			System.out.println("funcionários, cerca de 76% têm menos de 25 anos de idade e estão em sua primeira experiência.");
+			System.out.println();
+			System.out.println("Somos uma das empresas que mais oferece chances de ascensão profissional. Pois, quem trabalha com a gente tem várias oportunidades de desenvolvimento. Incentivamos o estudo e o aprimoramento, e");
+			System.out.println("temos orgulho de dizer que hoje aqui no Brasil, mais da metade dos gerentes ingressaram na companhia como atendentes, o primeiro degrau na escala hierárquica da empresa. Anualmente, são investidos cerca");
+			System.out.println("de R$40 milhões em treinamento e desenvolvimento de funcionários.");
+			System.out.println();
+			System.out.println("Aqui, todos têm espaço e oportunidade, não admitimos discriminação e exigimos o mesmo respeito para todos os nossos funcionários, sem qualquer distinção de sexo, idade, raça, orientação sexual, religião ou");
+			System.out.println("aparência física.");
+			System.out.println();
+			System.out.println("Quer fazer parte da nossa equipe?");
+			System.out.println("• Se você procura uma oportunidade como Atendente ou Jovem Aprendiz no restaurante McDonald’s, clique aqui para cadastrar seu currículo.");
+			System.out.println("• Para se candidatar a outras oportunidades na área corporativa e programa de estágio do McDonald’s, por favor, clique aqui.");
+			System.out.println("• Agora, se você tem algum tipo de deficiência, será muito bem-vindo neste time, clicando aqui.");	
+			
+			
+			System.out.println("POLÍTICA DE PRIVACIDADE");
+			System.out.println("Acesse o site  https://www.mcdonalds.com.br/quemsomos/privacidade para maiores informações");
+			
+			
+		
+			
+			System.out.println("RECEITA DO FUTURO");
+			System.out.println();
+			System.out.println("Na Arcos Dorados, temos o compromisso de ser uma parte ativa da solução aos desafios que nossa sociedade enfrenta atualmente, sempre trazendo propostas inovadoras. Dessa forma, nasceu nossa estratégia");
+			System.out.println("socioambiental “Receita do Futuro”, promovida com o objetivo de impactar positivamente os setores onde temos mais oportunidades de alcançar mudanças significativas. A \"Receita do Futuro\" está no centro ");
+			System.out.println("dos nossos valores e direciona a maneira de fazermos a diferença no meio ambiente e nas comunidades onde estamos presentes, ao lado de milhões de clientes, funcionários, fornecedores e empresas do setor.");
+			System.out.println();
+			System.out.println("Pilares de atuação:");
+			System.out.println("Como empresa líder na região, assumimos a responsabilidade de atuar em seis frentes prioritárias.");
+			System.out.println("Emprego Jovem");
+			System.out.println("Somos o maior empregador de jovens da América Latina, dando a oportunidade a milhares de pessoas de conquistarem seu primeiro emprego formal.");
+			System.out.println("Mudanças Climáticas");
+			System.out.println("Implementamos ações para minimizar os impactos ambientais em toda nossa cadeia de operações.");
+			System.out.println("Embalagem e Reciclagem");
+			System.out.println("Priorizamos a utilização de materiais renováveis e buscamos promover a mudança de antigos hábitos em nossos restaurantes.");
+			System.out.println("Fornecimento Sustentável");
+			System.out.println("Promovemos o bem-estar animal e buscamos ingredientes produzidos com respeito ao meio-ambiente.");
+			System.out.println("Família e Bem-estar");
+			System.out.println("Mantemos nosso compromisso com as famílias, evoluindo as opções de nosso menu com foco em oferecer mais qualidade de vida.");
+			System.out.println("Diversidade e Inclusão");
+			System.out.println("Promovemos equipes diversificadas e um ambiente de trabalho que estimule o respeito e incentive a participação de todas as pessoas, favorecendo a inclusão e a igualdade de oportunidades.");
+					
+					
+			
+				
+			System.out.println("PUBLICIDADE RESPONSÁVEL E CRITÉRIOS NUTRICIONAIS");
+			System.out.println();
+			System.out.println("Em 2007, o McDonald's tornou público seu código de ética publicitária no Brasil. O objetivo dessa iniciativa foi o de tornar público o compromisso da empresa no tocante à publicidade da marca.");
+			System.out.println();
+			System.out.println("Em complementação a estas práticas, em 2009 o McDonald's Brasil, através da Arcos Dorados  que possui a licença para operar a marca no Brasil, assinou, juntamente com 22 outras empresas precursoras, o");
+			System.out.println("compromisso brasileiro elaborado pela Associação Brasileira de Anunciantes (ABA) em conjunto com a Associação Brasileira das Indústrias de Alimentos (ABIA) sobre publicidade dirigida ao público infantil em");
+			System.out.println("complemento às normas anteriormente estabelecidas pelo CONAR (Conselho Nacional de Auto-regulamentação Publicitária).");
+			System.out.println();
+			System.out.println("Em 2016, em conjunto com um grupo de empresas do setor de alimentos e bebidas no Brasil, assumimos o compromisso de ampliar nosso papel na política de marketing e publicidade responsável para crianças,");
+			System.out.println("com avanços nas diretrizes estabelecidas em 2009, por meio da padronização de critérios nutricionais únicos e da estruturação de um processo de monitoramento independente e imparcial. Mais informações");
+			System.out.println("em: http://www.publicidaderesponsavel.com.br/. Importante destacar que somos o único restaurante deste compromisso.");
+			System.out.println();
+			System.out.println("No tema, aqui no Brasil, ainda seguimos os compromissos globais do McDonald’s (clique aqui e conheça – disponível apenas em inglês) e, localmente, os compromissos da Arcos Dorados em toda América Latina");
+			System.out.println("(clique aqui e saiba mais).");
+			System.out.println();
+			System.out.println("E, por falar em critérios nutricionais, desde 2011, a Arcos Dorados, que sempre teve um cuidado especial com a composição nutricional do seu menu infantil, passou a ofertar produtos diferentes do seu cardápio");
+			System.out.println("tradicional, tendo já oferecido ao longo dos anos cenourinha, maçã, suco e purê de maçã 100% fruta.");
+			System.out.println();
+			System.out.println("Em 2019, tivemos a mais recente mudança nutricional no Combo do McLanche Feliz: com opções de diferentes grupos alimentares, redução das gorduras, do sódio e do açúcar adicionado e trabalhando apenas");
+			System.out.println("com corantes e aromatizantes naturais.");
+			System.out.println();
+			System.out.println("O menu sugerido após a mudança conta com sanduíche com presença de vegetais, petit suisse sem corante e aromatizantes artificias como sobremesa, e água e as McFritas Kids. Nossa prioridade é servir");
+			System.out.println("comida de qualidade, gerando momentos deliciosos. É por isso que assumimos um forte compromisso não só em relação à publicidade, mas também com o cardápio que oferecemos e com as famílias que");
+			System.out.println("consomem nossos produtos todos os dias.");
+			
+			
+		}
+			
+
+	}
+  
+  

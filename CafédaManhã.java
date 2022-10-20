993e1c437545f0566f8e2d364bbc06ea93e77b00
GabrielaFurtado <115599756+GabrielaFurtado@users.noreply.github.com>
2022-10-19 22:08:13 -0300

    Create CafédaManhã.java

---
+++ CafédaManhã.java
@@ -0,0 +1,28 @@
+# ProjetoMequi
+package projetoMequi;
+
+import java.util.Scanner;
+
+public class CafédaManhã {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+	
+		System.out.println ("Selecione a opção Café da Manhã");
+		System.out.println  ("Queijo Quente");
+		System.out.println  ("Croissant de Presunto e Queijo");
+		System.out.println  ("Pão de Queijo");
+		System.out.println  ("Capuccino 300ml");
+		System.out.println  ("Capuccino 200ml");
+		System.out.println  ("Café com Leite 200ml");
+		System.out.println ("Café com Leite 300ml");
+		System.out.println ("Café Premium 300ml");
+		System.out.println  ("Café Premium 100ml");
+		System.out.println ("Café Premium 200ml");
+		System.out.println ("Chocolate Quente 300ml");
+		System.out.println ("Chocolate Quente 200ml");
+
+	}
+
+}

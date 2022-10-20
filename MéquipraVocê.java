4dab801445ffcedd1c26a00f81fca864cdd71eff
GabrielaFurtado <115599756+GabrielaFurtado@users.noreply.github.com>
2022-10-20 00:16:05 -0300

    Create MéquipraVocê.java

---
+++ MéquipraVocê.java
@@ -0,0 +1,202 @@
+# ProjetoMequi
+package projetoMequi;
+
+import java.util.Scanner;
+
+public class MéquipraVoce {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new  Scanner(System.in);
+		
+		 System.out.println ("Selecione a opção Mequi pra você"); 
+
+		   System.out.println ("App do Méqui"); 
+
+		   System.out.println("McDelivery"); 
+
+		   System.out.println ("Méqui no Ifood"); 
+
+		   System.out.println(" Peça e retire"); 
+
+		   System.out.println("Vale Méqui"); 
+
+		   System.out.println ("Méqui Zap"); 
+
+		   System.out.println("Drive-tudo"); 
+
+		   System.out.println ("Portas Abertas"); 
+
+		   System.out.println ("Projeto Grafite"); 
+
+	   int opcao1=sc.nextInt (); 
+
+    
+
+	   switch (opcao1) { 
+
+	    
+
+	   case 1: 
+		   
+		  System.out.println ("App do Méqui");
+		   System.out.println ("São várias promoções com até 50% de desconto pra você aproveitar muito pagando pouco.");
+		   
+		   int opcao2=sc.nextInt (); 
+
+		    
+
+		   switch (opcao2) { 
+
+		    
+
+		   case 2: 
+			   
+			System.out.println ("Méqui Delivery") ;
+			System.out.println ("#MéquiNoSofá");
+			System.out.println ("Peça e receba em casa, no trabalho ou onde quiser");
+			
+			
+			int opcao3=sc.nextInt (); 
+
+		    
+
+			   switch (opcao3) { 
+
+			    
+
+			   case 3: 
+				   
+				   
+			System.out.println ("Méqui no Ifood");
+			System.out.println ("Restaurantes Participantes");
+			System.out.println ("Descubra o Méqui mais próximo de você");
+			
+			int opcao4=sc.nextInt (); 
+
+		    
+
+			   switch (opcao4) { 
+
+			    
+
+			   case 4: 
+				   
+				   
+			System.out.println ("Peça e retire");
+			System.out.println ("Peça e pague pelo App");
+			System.out.println ("Retire no Méqui");
+			System.out.println ("Facinho de usar. Confere aqui no passo a passo abaixo:");
+			System.out.println ("1-Baixe o App do Méqui");
+			System.out.println ("2-Entre no Peça e Retire");
+			System.out.println ("3-Faça seu pedido e pague pelo App");
+			System.out.println ("Quando estiver chegando no restaurante, aperte o botão começar a preparar.");
+			System.out.println ("Escolha como quer retirar seu pedido e aproveite!");
+
+			int opcao5=sc.nextInt (); 
+
+		    
+
+			   switch (opcao5) { 
+
+			    
+
+			   case 5:
+				   
+			System.out.println ("Vale Méqui") ;
+			System.out.println ("1-Escolha o valor");
+			System.out.println("2-Receba o Vale Méqui");
+			System.out.println ("3-Presenteie quem quiser");
+			
+			
+			int opcao6=sc.nextInt (); 
+
+		    
+
+			   switch (opcao6) { 
+
+			    
+
+			   case 6:
+				   
+				   
+			System.out.println ("Méqui Zap");
+			System.out.println ("Méqui seu novo contatinho");
+			System.out.println ("Já Add o Méqui no zap?");
+			System.out.println ("Aquele contatinho que nunca te deixa no vácuo e sempre manda oferta boa é o nosso");
+			System.out.println ("Envia uma mensagem");
+			System.out.println ("Restaurantes participantes");
+			System.out.println ("Veja qual Méqui aceita os cupons que você vai pedir pelo ZAP.");
+			System.out.println ("Clique aqui");
+			
+			
+
+			int opcao7=sc.nextInt (); 
+
+		    
+
+			   switch (opcao7) { 
+
+			    
+
+			   case 7:
+				   
+				   
+			System.out.println("Drive tudo no Méqui");
+			System.out.println ("Méqui VIP Drive");
+			System.out.println ("Descontos exclusivos e todo o conforto pra você dar aquela passadinha no Méqui.");
+			System.out.println ("Drive-Thru virou Drive-tudo do Méqui");
+			System.out.println ("No Drive Tudo agora vale bike, skate, moto, patinete, o que você quiser");
+			
+			
+			int opcao8=sc.nextInt (); 
+
+		    
+
+			   switch (opcao8) { 
+
+			    
+
+			   case 8:
+				   
+				   
+		System.out.println ("Portas Abertas");
+		System.out.println ("O programa Portas Abertas foi criado para mostrar as boas práticas que fazem parte do dia a dia nos restaurantes da rede");
+		System.out.println("Uma visita guiada permite conhecer mais sobre os rígidos protocolos de higiene, segurança e controle de qualidade das nossas cozinhas.");
+		System.out.println ("No Brasil, mais de 10 milhões de pessoas já participaram da visita");
+		System.out.println ("Na América Latina o programa já recebeu mais de 15 milhões de visitantes, distribuídos em 20 países.");
+		System.out.println ("Por conta da Pandemia, o Portas Abertas mudou e os restaurantes ganharam");
+		System.out.println ("reforço do programa McProtegidos, que conta com uma");
+		System.out.println(" série de medidas adicionais de higiene e segurança para estimular ambientes mais seguros a todos.");
+		System.out.println ("Para continuar apresentando de forma transparente as novidades");
+		System.out.println	(" e como os restaurantes funcionam, o Phellyx te convida para conhecer a cozinha do McDonald's virtualmente! Clique aqui");
+
+		int opcao9=sc.nextInt (); 
+
+	    
+
+		   switch (opcao9) { 
+
+		    
+
+		   case 9:
+		
+		System.out.println ("Restaurante Drive Lago Sul");
+		System.out.println ("Brasília / DF - SMDB, Conj. 12, Lote 2 - Setor de Mansões Dom Bosco");)
+		System.out.println ("ARTISTA: SALVE MUROS");
+		System.out.println ("MOBILIDADE É: “Mobilidade é a facilidade de se deslocar até o McDonald’s de formas diferentes.");
+		System.out.println ("O QUE MOVE SALVE MUROS?");
+		System.out.println ("Grafiteiro, artista urbano, ilustrador, integrante do Coletivo Salve os Muros e ainda membro do podcast Salve os Muros. ");
+		System.out.println ("O QUE MOVEU A ARTE NO MÉQUI?")
+		System.out.println("Buscamos desenvolver uma narrativa que envolvesse características e elementos de Brasília se relacionando aos produtos produzidos no restaurante. As cores pensadas para a produção do painel fazem alusão tanto as cores do McDonald’s quanto as cores do pôr do sol do céu de Brasília, além de acrescentar objetos que remetessem a mobilidade urbana presente na cidade.");
+		System.out.println ("A ARTE NO DRIVE-TUDO")
+		System.out.println ("A inspiração veio da nossa relação com a cidade de Brasília, com as vivências nas ruas da cidade, suas cores, texturas, artistas. Nosso dia a dia pintando em diversos cantos e que muitas vezes acabavam com uma parada em um dos restaurantes da rede. Ponto principal do mural é a junção dos elementos de Brasília (Ipê), Mobilidade (Camelo) e McDonald’s (Hambúrguer e McFritas), de modo a fazer com que dialoguem, formando assim uma composição única.");
+		System.out.println ("Camelo: gíria de Brasília para bicicleta");
+		System.out.println ("A MÉQUIZICE DE SALVE MUROS:");
+		System.out.println ("Minha Méquizice é um McShake com Mcfritas.");
+    
+         }
+    
+    }
+    
+    }

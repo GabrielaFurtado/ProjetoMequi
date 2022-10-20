e57bdf1c0944b9ee9eb0db3871e9f7606456d190
GabrielaFurtado <115599756+GabrielaFurtado@users.noreply.github.com>
2022-10-19 21:59:54 -0300

    Create SanduíchesPremium.java

---
+++ SanduíchesPremium.java
@@ -0,0 +1,17 @@
+# ProjetoMequi
+package projetoMequi;
+
+import java.util.Scanner;
+
+public class SanduichesPremium {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println ("Selecione a opção Sanduíches Premium");
+	    System.out.println ("McClub");
+
+	}
+
+}

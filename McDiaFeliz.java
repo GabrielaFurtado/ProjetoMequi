4b45d8ac7e7a6cfff08f613c31f9e090ecb1d6e0
GabrielaFurtado <115599756+GabrielaFurtado@users.noreply.github.com>
2022-10-20 00:25:14 -0300

    Create McDiaFeliz.java

---
+++ McDiaFeliz.java
@@ -0,0 +1,86 @@
+# ProjetoMequi
+package projetoMequi;
+
+import java.util.Scanner;
+
+public class McDiaFeliz {
+
+	public static void main(String[] args) {
+		Scanner sc=new  Scanner(System.in);
+		
+		System.out.println ("Selecione a opção McDiaFeliz");
+		System.out.println ("Sobre a causa");
+		System.out.println ("Participe");
+		System.out.println ("Contato");
+		System.out.println ("Regulamento e Restaurantes participantes");
+		System.out.println ("Receita do futuro");
+		
+		int opcao1=sc.nextInt ();  
+		
+		switch (opcao1) {
+
+		case 1:
+
+			System.out.println ("Sobre a causa");
+			System.out.println ("Leia em:");
+			System.out.println("https://www.mcdonalds.com.br/mcdia-feliz/sobre-a-causa");
+			
+			
+			int opcao2=sc.nextInt ();  
+			
+			switch (opcao2) {
+					
+
+			case 2:
+				
+		 System.out.println ("1 - Sobre a causa | 2 - Contato | 3 - Receita para o futuro");
+		 
+		 
+		 int opcao3=sc.nextInt ();  
+			
+			switch (opcao3) {
+					
+
+			case 3:
+				
+			System.out.println ("Sobre a causa");
+			System.out.println("https://www.mcdonalds.com.br/mcdia-feliz/sobre-a-causa");
+			
+         int opcao4=sc.nextInt ();  
+			
+			switch (opcao4) {
+					
+
+			case 4:
+				
+			System.out.println("Contato SAC McD");
+			System.out.println("sac@sacmcdonalds.com.br");
+			System.out.println("Contato IRM");
+			System.out.println("mcdiafeliz@instituto-ronald.org.br");
+			System.out.println("Contato IAS");
+			System.out.println("mcdiafeliz@ias.org.br");
+			
+			
+       int opcao5=sc.nextInt ();  
+			
+			switch (opcao5) {
+					
+
+			case 5:
+				
+				System.out.println("Receita para o Futuro");
+				System.out.println();
+				System.out.println("O McDia Feliz é uma das maiores campanhas de arrecadação de fundos para as causas de saúde e educação do país e, hoje, essa iniciativa faz parte de uma consolidada estratégia socioambiental da Arcos");
+				System.out.println("Dorados (link – www.arcosdorados.com), franquia operadora da marca McDonald’s em 20 países da América Latina e Caribe.");
+				System.out.println();
+				System.out.println("Com o propósito de gerar impacto positivo no planeta e na sociedade, a Receita do Futuro é a plataforma ESG da companhia, que engloba os principais compromissos, iniciativas e avanços em seis pilares");
+				System.out.println("principais, sendo eles: Economia Circular, Mudanças Climáticas, Abastecimento Sustentável, Emprego Jovem, Família & Bem-Estar e Diversidade & Inclusão.");
+				System.out.println();
+				System.out.println("Nesse sentido, o McDia Feliz é uma campanha que reforça e nos impulsiona a realizar profundas transformações na vida de milhares de famílias e, permite que por meio desse compromisso, possamos mobilizar");
+				System.out.println("e impactar também as comunidades onde estamos presentes. Por isso, anualmente contamos com o apoio e a participação de toda a sociedade para continuar beneficiando milhares de crianças e jovens.");
+				System.out.println("Para conhecer mais sobre as nossas iniciativas socioambientais, acesse\nwww.receitadofuturo.com.br ");
+				
+				
+			     }
+			}
+		}

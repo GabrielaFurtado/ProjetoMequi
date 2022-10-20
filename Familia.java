59e7d8f34298d0ce9f3cd39842c6a6067f651914
GabrielaFurtado <115599756+GabrielaFurtado@users.noreply.github.com>
2022-10-19 21:52:05 -0300

    Update Familia.java

--- Familia.java
+++ Familia.java
@@ -1,1 +1,77 @@
 # ProjetoMequi
+package projetoMequi;
+
+import java.util.Scanner;
+
+public class Familia {
+
+	public static void main(String[] args) {
+		
+        Scanner sc=new  Scanner(System.in);
+		
+		System.out.println ("Nova rota Méqui");
+		System.out.println ("Que tal juntar a família pra ter um momento de muita diversão no Méqui com os Carros na Estrada? ");
+		
+        
+			System.out.println("Aventureiro Relâmpago McQueen");
+			System.out.println();
+			System.out.println("Coloque o Aventureiro Relâmpago McQueen sobre uma superfície plana. Deslize-o");
+			System.out.println("para trás e depois solte-o, para ver como sai lançado para a frente, fazendo ");
+			System.out.println("uma curva à direita.Vem com uma folha de adesivos.");
+			
+			
+		System.out.println("Cruz Ramirez");
+		System.out.println();
+		System.out.println("Coloque o Cruz Ramirez sobre uma superfície plana. Deslize-o");
+		System.out.println("para trás e depois solte-o, para ver como sai lançado para a frente, fazendo ");
+		System.out.println("uma curva à direita.Vem com uma folha de adesivos.");
+		
+		System.out.println("Mate");
+		System.out.println();
+		System.out.println("Coloque o Mate sobre uma superfície plana. Deslize-o");
+		System.out.println("para trás e depois solte-o, para ver como sai lançado para a frente, fazendo ");
+		System.out.println("uma curva à direita.Vem com uma folha de adesivos.");
+		
+			
+		System.out.println("Relâmpago McQueen");
+		System.out.println();
+		System.out.println("Coloque o Relâmpago McQueen sobre uma superfície plana. Deslize-o");
+		System.out.println("para trás e depois solte-o, para ver como sai lançado para a frente, fazendo ");
+		System.out.println("uma curva à direita.Vem com uma folha de adesivos.");
+			
+			
+		System.out.println("Aventureiro Mate");
+		System.out.println();
+		System.out.println("Coloque o Aventureiro Mate sobre uma superfície plana. Deslize-o");
+		System.out.println("para trás e depois solte-o, para ver como sai lançado para a frente, fazendo ");
+		System.out.println("uma curva à direita.Vem com uma folha de adesivos.");
+			
+			
+			System.out.println("Ivy");
+			System.out.println();
+			System.out.println("Coloque o Ivy sobre uma superfície plana. Deslize-o");
+			System.out.println("para trás e depois solte-o, para ver como sai lançado para a frente, fazendo ");
+			System.out.println("uma curva à direita.Vem com uma folha de adesivos.");
+			
+			
+			System.out.println("Homem das cavernas Relâmpago McQueen");
+			System.out.println();
+			System.out.println("Coloque o Homem das cavernas Relâmpago McQueen sobre uma superfície plana. Deslize-o");
+			System.out.println("para trás e depois solte-o, para ver como sai lançado para a frente, fazendo ");
+			System.out.println("uma curva à direita.Vem com uma folha de adesivos.");
+			
+			
+		System.out.println("Homem das cavernas Mate");
+		System.out.println();
+		System.out.println("Coloque o Homem das cavernas Mate sobre uma superfície plana. Deslize-o");
+		System.out.println("para trás e depois solte-o, para ver como sai lançado para a frente, fazendo ");
+		System.out.println("uma curva à direita.Vem com uma folha de adesivos.");
+		
+			
+		System.out.println ("McLanche Feliz");
+		System.out.println ("Você sabe o que é McLanche Feliz?");
+		System.out.println ("É um cardápio infantil variado que atende aos critérios nutricionais");
+		System.out.println (" globais da empresa, composto por: um sanduíche");
+		System.out.println 	("ou Chicken McNuggets, um acompanhamento");
+		System.out.println ("sendo McFritas Kids ou Tomatinho), uma sobremesa (Petit Suisse) e uma bebida (água mineral 500 ml"); 
+		System.out.println  ("Além disso, inclui um brinquedo.");

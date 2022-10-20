d076515e9125f59861ff81cdd09e32c75497ec25
GabrielaFurtado <115599756+GabrielaFurtado@users.noreply.github.com>
2022-10-20 00:27:04 -0300

    Create McOferta.java

---
+++ McOferta.java
@@ -0,0 +1,19 @@
+# ProjetoMequi
+package projetoMequi;
+
+import java.util.Scanner;
+
+public class McOferta {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println ("Selecione a opção McOferta");
+		System.out.println  ("BigMac");
+		System.out.println  ("Quarterão com Queijo");
+		System.out.println  ("Cheddar McMelt");
+	
+	}
+
+}

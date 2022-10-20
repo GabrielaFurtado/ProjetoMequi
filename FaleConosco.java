d45faf9c72adaefb78d933f497fa6730dd41f175
GabrielaFurtado <115599756+GabrielaFurtado@users.noreply.github.com>
2022-10-19 22:11:58 -0300

    Create FaleConosco.java

---
+++ FaleConosco.java
@@ -0,0 +1,38 @@
+# ProjetoMequi
+package projetoMequi;
+
+import java.util.Scanner;
+
+
+public class FaleConosco {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new  Scanner(System.in);
+		System.out.println ("Selecione a opção Fale Conosco"); 
+
+		  System.out.println ("CONTATO"); 
+
+		  System.out.println ("ATENÇÃO: Caso queira enviar seu currículo ou consultar vagas de trabalho disponíveis, clique aqui."); 
+
+		     System.out.println ("Preencha o formulário abaixo e clique em Enviar"); 
+
+		     System.out.println ("Campos Obrigatórios"); 
+
+		     System.out.println ("\nNome"); 
+
+		     System.out.println ("\nSobrenome"); 
+
+		     System.out.println ("\nE-mail"); 
+
+		     System.out.println ("\nTelefone"); 
+
+		     System.out.println ("\nCidade"); 
+
+		     System.out.println ("\nAssunto"); 
+
+		     System.out.println ("\nMensagem"); 
+
+	}
+
+}

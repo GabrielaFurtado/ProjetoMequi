a742fa6e26f1c6e458a9a53c0d5c09f03d4ea236
GabrielaFurtado <115599756+GabrielaFurtado@users.noreply.github.com>
2022-10-19 21:57:06 -0300

    Update cardapio.java

--- cardapio.java
+++ cardapio.java
@@ -10,63 +10,63 @@ public class Cardapio {
 	
 	Scanner sc = new Scanner(System.in);
     
-    System.out.println(" 1-Lançamento");  
+       System.out.println(" 1-Lançamento");  
 
   
 
-System.out.println("2- Sanduíches Premium");  
+     System.out.println("2- Sanduíches Premium");  
 
   
 
-System.out.println("3- Sanduíches de Carne Bovina");  
+   System.out.println("3- Sanduíches de Carne Bovina");  
 
   
 
-System.out.println("4- Famiília Tasty"); 
+   System.out.println("4- Famiília Tasty"); 
 
   
 
-System.out.println("5- Sanduíches de Frango");  
+   System.out.println("5- Sanduíches de Frango");  
 
   
 
-System.out.println("6- Seleção Mequi");  
+   System.out.println("6- Seleção Mequi");  
 
   
 
-System.out.println("7- McLanche Feliz");  
+  System.out.println("7- McLanche Feliz");  
 
   
 
-System.out.println("8- Mequi1000");  
+  System.out.println("8- Mequi1000");  
 
   
 
-System.out.println("9-Acompanhamentos");  
+ System.out.println("9-Acompanhamentos");  
 
   
 
-System.out.println("10-Sobremesas");  
+  System.out.println("10-Sobremesas");  
 
   
 
-System.out.println ("11-Bebidas Frias");  
+  System.out.println ("11-Bebidas Frias");  
 
   
 
-System.out.println("12-Café da Manhã");  
+  System.out.println("12-Café da Manhã");  
 
   
 
-System.out.println("13-Bebidas Quentes"); 
+  System.out.println("13-Bebidas Quentes"); 
 
  
 
-System.out.println("14-McOferta"); 
+ System.out.println("14-McOferta"); 
 
  
 
-System.out.println("15-MéquiBox"); 
+  System.out.println("15-MéquiBox"); 
     
   }
   

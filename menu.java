6df1673e0cc5882e1c0da249555bff067273fe13
GabrielaFurtado <115599756+GabrielaFurtado@users.noreply.github.com>
2022-10-19 21:53:03 -0300

    Update menu.java

--- menu.java
+++ menu.java
@@ -1,1 +1,39 @@
+package projetoMequi;
 
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		
+			
+			Scanner sc=new Scanner(System.in); 
+
+			System.out.println("Mequi"); 
+
+			System.out.println ("Bateu aquela Fome de Méqui");
+
+			System.out.println (); 
+
+			System.out.println ("Selecione a sua opção:"); 
+
+			System.out.println(); 
+
+			System.out.println("1-Cardápio"); 
+
+			System.out.println ("2-Lançamentos"); 
+
+			System.out.println ("3-Quem Somos"); 
+
+			System.out.println ("-4-Famiília"); 
+
+			System.out.println ("5-Promoções"); 
+
+			System.out.println ("6-Mequi pra você"); 
+
+			System.out.println("7-McDia Feliz"); 
+			
+
+	}
+
+}

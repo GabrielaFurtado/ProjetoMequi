26f535513784d1fd7f523b562c6684b683d2f13a
GabrielaFurtado <115599756+GabrielaFurtado@users.noreply.github.com>
2022-10-19 22:09:17 -0300

    Create Méqui1000.java

---
+++ Méqui1000.java
@@ -0,0 +1,18 @@
+# ProjetoMequi
+package projetoMequi;
+
+import java.util.Scanner;
+
+public class Méqui1000 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println ("Selecione a opção Méqui1000");
+		System.out.println  ("SuperMc");
+		System.out.println  ("McRings Cheddar Bacon");
+		System.out.println  ("Caldo&Freddo Chocolate");
+		System.out.println  ("Caldo&Freddo Morango");
+		System.out.println  ("Caldo&Freddo Caramelo");
+		System.out.println  ("McRings Bacon");

c4197153c6784895091c3886b1d3cecbdf781920
GabrielaFurtado <115599756+GabrielaFurtado@users.noreply.github.com>
2022-10-19 22:10:36 -0300

    Create Promocoes.java

---
+++ Promocoes.java
@@ -0,0 +1,33 @@
+# ProjetoMequi
+package projetoMequi;
+
+import java.util.Scanner;
+
+public class Promocoes {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new  Scanner(System.in);
+		
+		System.out.println ("Selecione a opção Promoções");
+		System.out.println ("Promoções Ifood");
+		System.out.println ("Promoções App");
+		
+		System.out.println ("Clássicos do Méqui no iFood");
+		System.out.println ("Peça os mais amados com um descontão");
+		System.out.println ("Peça aqui");
+		
+		System.out.println ("Até 43% OFF com iFood");
+		System.out.println ("Todo dia tem um desconto especial no delivery");
+		System.out.println ("Peça aqui");
+		
+		System.out.println ("Fome geladinha de Méqui no sofá?");
+		System.out.println	("Vem de McDelivery!");
+
+	
+	   System.out.println ("Promoçoes App");
+	   System.out.println ("Partiu App?");
+	   System.out.println ("Aproveite os descontos exclusivos de até 50% de desconto");
+	   
+	}
+	}

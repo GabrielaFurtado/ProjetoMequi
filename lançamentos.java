b7ea5460e342341c2f5d6d60e6f85b86c9a41e5f
GabrielaFurtado <115599756+GabrielaFurtado@users.noreply.github.com>
2022-10-19 21:58:40 -0300

    Update lançamentos.java

--- lançamentos.java
+++ lançamentos.java
@@ -1,1 +1,87 @@
 # ProjetoMequi
+package projetoMequi;
+
+
+
+public class Lançamento {
+	
+
+	public static void main(String[] args) {
+		
+		Scanner sc =new  Scanner(System.in); 
+		
+
+		
+		System.out.println("Selecione a opção Lançamentos");
+		
+		System.out.println ("Seleções do Méqui");
+	
+		System.out.println ("Bora provas as Seleções do Méqui?");
+		System.out.println ("Pede seu McDeleverye experimente!");
+		System.out.println ("São 8 países diferentes e você pode provar aí de sua casa.\n É só pedir um McDelivery.\n Bora curtir as Seleções do Méquino no sofá?");
+		System.out.println ("Peça já");
+		
+		
+      int opcao1=sc.nextInt (); 
+		
+		switch (opcao1) {
+		
+		case 1:
+			
+	 System.out.println ("MéquiVipDrive");
+	 System.out.println ("Passe no Drive-Tudo e peça já o seu sanduíche favorito das Seleções do Méqui. Vem com tudo.");
+	 System.out.println ("Venha agora!");
+	 
+	 int opcao2=sc.nextInt (); 
+		
+		switch (opcao2) {
+		
+		case 2:
+			
+	  System.out.println ("Peça e retire");
+	  System.out.println ("Prove as Seleções do Méquino Peça e Retire.\n Experimente");
+	  System.out.println ("Prove agora");
+	  
+	  int opcao3=sc.nextInt (); 
+		
+		switch (opcao3) {
+		
+		case 3:
+			
+	 System.out.println ("Segunda-feira McCatar");
+	 System.out.println ("Terça-feira McMéxico");
+	 System.out.println ("Quarta-feira McFrança");
+	 System.out.println ("Quinta-feira McAlemanha");
+	 System.out.println ("Sexta-feira McEspanha");
+	 System.out.println ("Sábado McArgentina");
+	 System.out.println ("Domingo McEua");
+	 System.out.println ("Todos os dias tem McBrasil");
+	 System.out.println ("Todos os dias tem McFluerry Brasil");
+	 break;
+	 int opcao4=sc.nextInt (); 
+		
+		switch (opcao4) {
+		
+		case 4:
+			
+		System.out.println("Baixe o App");
+		System.out.println ("Fique sempre por dentro das novidades");
+		System.out.println ("Só quem tem o app fica por dentro das novidades antes de todo mundo, hein!!");
+		System.out.println ("1 - Android | 2 - IOS\"");
+		break;
+		
+        int opcao5=sc.nextInt (); 
+		
+		switch (opcao5) {
+		
+		case 5:
+			
+			System.out.println("Restaurantes participantes");
+			System.out.println("");
+			System.out.println("Digite o seu endereço ou sua cidade: ");
+			System.out.println("Loja mais proxima a 1km");
+			break;
+			
+		     }
+		
+		}

9482d0acc701781450664fffc53543bf9385aa0b
GabrielaFurtado <115599756+GabrielaFurtado@users.noreply.github.com>
2022-10-19 22:02:54 -0300

    Create SanduichesFrango.java

---
+++ SanduichesFrango.java
@@ -0,0 +1,24 @@
+# ProjetoMequi
+package projetoMequi;
+
+import java.util.Scanner;
+
+public class SanduichesFrango {
+
+	public static void main(String[] args) {
+		
+
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println ("Selecione a opção Sanduíches de Frango");
+        System.out.println ("McCrispy Chicken Deluxe");
+        System.out.println  ("McCrispy Chicken Classic");
+        System.out.println  ("McCrispy Chicken Spicy");
+        System.out.println  ("McChicken Bacon");
+        System.out.println  ("McChicken");
+        System.out.println  ("Chicken Jr");
+
+	}
+
+}

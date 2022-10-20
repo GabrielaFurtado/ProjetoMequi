033a7eabcef849413de66679b576921850d11b40
GabrielaFurtado <115599756+GabrielaFurtado@users.noreply.github.com>
2022-10-19 22:01:20 -0300

    Create SanduíchesCarneBovina.java

---
+++ SanduíchesCarneBovina.java
@@ -0,0 +1,27 @@
+# ProjetoMequi
+package projetoMequi;
+
+import java.util.Scanner;
+
+
+
+public class SanduichesCarneBovina {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println ("Selecione a opção Sandúiches de Carne Bovina");
+        System.out.println ("Duplo Quarterão");
+        System.out.println  ("Quarterão com Queijo");
+        System.out.println  ("McNífico Bacon");
+        System.out.println  ("Duplo Cheddar McMelt");
+        System.out.println  ("CheddarMcMelt");
+        System.out.println  ("Duplo Burger Bacon");
+        System.out.println  ("Duplo Burguer com Queijo");
+        System.out.println ("Triplo Cheeeburguer");
+        System.out.println ("McFiesta");
+        System.out.println ("Cheeseburguer");
+
+	}
+
+}

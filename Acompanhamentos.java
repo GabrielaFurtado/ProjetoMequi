f624b8991bdbb27629ba1d51feae300214ba0bfd
GabrielaFurtado <115599756+GabrielaFurtado@users.noreply.github.com>
2022-10-19 22:04:57 -0300

    Create Acompanhamentos.java

---
+++ Acompanhamentos.java
@@ -0,0 +1,35 @@
+# ProjetoMequi
+package projetoMequi;
+
+import java.util.Scanner;
+
+public class Acompanhamentos {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println ("Selecione a opção Acompanhamentos");
+		System.out.println  ("McFritas Cheddar Bacon");
+		System.out.println  ("McFritas Grande");
+		System.out.println  ("McFritas Média");
+		System.out.println  ("McFritas Pequena");
+		System.out.println  ("McFritas Kids");
+		System.out.println  ("Chicken McNuggets 6 unidades");
+		System.out.println ("Chicken McNuggets 4 unidades");
+		System.out.println ("Chicken McNuggets 10 unidades");
+		System.out.println  ("Molho Agridoce");
+		System.out.println ("Molho Barbecue");
+		System.out.println ("Molho Ranch");
+		System.out.println ("Molho Caipira");
+		System.out.println ("Mega McNuggets");
+		System.out.println ("Ketchup");
+		System.out.println ("Mostarda");
+		System.out.println ("Tomatinho");
+		System.out.println ("Mega McFritas");
+		System.out.println ("Molho Blister CBO");
+		System.out.println ("Molho Blister Tasty");
+
+	}
+
+}
